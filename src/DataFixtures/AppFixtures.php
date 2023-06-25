fbd615577a180a4178de76691e9303a1b1dc8e36
Guillaume Ganne <guillaumeganne.dev@gmail.com>
2023-06-25 09:28:59 +0200

    user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,12 +4,14 @@ namespace App\DataFixtures;
 
 
 use Faker\Factory;
+use App\Entity\User;
 use Faker\Generator;
-use App\Entity\Auteurs;
 
+use App\Entity\Auteurs;
 use App\Entity\Citations;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -18,13 +20,18 @@ class AppFixtures extends Fixture
      */
     private Generator $faker;
 
-    public function __construct()
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
     {
         $this->faker = Factory::create('fr_FR');
+        $this->hasher = $hasher;
     }
     
     public function load(ObjectManager $manager): void
     {
+
+        // auteurs
         for ($i = 0; $i < 20; $i++) {
             $auteur = new Auteurs();
             $auteur->setAuteur($this->faker->name());
@@ -32,6 +39,7 @@ class AppFixtures extends Fixture
             $manager->persist($auteur);
         }
 
+        // citations
         for ($i = 0; $i < 100; $i++) {
             $citation = new Citations();
             $citation->setCitation($this->faker->text(127));
@@ -39,6 +47,31 @@ class AppFixtures extends Fixture
             $citation->setAuteurs($auteur);
             $manager->persist($citation);
         }
+
+        // Utilisateurs
+        $user = new User();
+        $userOne = new User();
+        $user->setEmail('guillaume.ganne@gmail.com');
+        $userOne->setEmail('test@test.com');
+
+        $userOne->setRoles(['ROLE_USER']);
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $hashPassword = $this->hasher->hashPassword(
+            $user,
+            '1234'
+        );
+
+        $hashPasswordOne = $this->hasher->hashPassword(
+            $userOne,
+            '1234'
+        );
+
+        $userOne->setPassword($hashPasswordOne);
+        $user->setPassword($hashPassword);
+
+        $manager->persist($user);
+        $manager->persist($userOne);
 
         $manager->flush();
     }
---
+++ src/Entity/User.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[UniqueEntity('email')]
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 320, unique: true)]
+    #[Assert\Email]
+    private string $email;
+
+    #[ORM\Column]
+    #[Assert\NotNull]
+    private array $roles = [];
+
+    private ?string $plainPassword = null;
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    #[Assert\NotBlank()]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    #[Assert\NotNull]
+    private ?\DateTimeImmutable $createdAt = null;
+
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(string $password): static
+    {
+        $this->plainPassword = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    
+}
