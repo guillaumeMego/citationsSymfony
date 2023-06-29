b71f7146a060389cce2138944713dfb2df1646c9
guillaumeMego <guillaumeganne.dev@gmail.com>
2023-06-29 08:15:50 +0200

    fini

---
+++ src/Controller/ApiController 2.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AuteursRepository;
+use App\Repository\CitationsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    /**
+     * Methode qui retourne la liste des citations
+     * 
+     * @param CitationsRepository $repository
+     * @return JsonResponse
+     */
+    #[Route('/api/citations', name: 'api', methods: ['GET'])]
+    public function index(
+        CitationsRepository $repository,
+        AuteursRepository $auteursRepository
+    ): JsonResponse {
+        $citations = $repository->findAll();
+        $auteursRepository->findAll();
+
+        $data = [];
+
+        foreach ($citations as $citation) {
+            $data[] = [
+                'id' => $citation->getId(),
+                'citation' => $citation->getCitation(),
+                'Explication' => $citation->getExplication(),
+                'date' => $citation->getDateModif(),
+                'auteur' => ($citation->getAuteurs() !== null) ? [
+                    'id' => $citation->getAuteurs()->getId(),
+                    'Auteur' => $citation->getAuteurs()->getAuteur(),
+                    'Bio' => $citation->getAuteurs()->getBio(),
+                ] : null,
+            ];
+        }
+
+        $response = new JsonResponse($data);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
+    }
+}
---
+++ src/Form/CitationsType 2.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Auteurs;
+use App\Entity\Citations;
+use App\Repository\AuteursRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CitationsType extends AbstractType
+{
+    /**
+     * Methode qui construit le formulaire
+     *
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     * @return void
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('citation', TextType::class, [
+                'label' => 'Citation',
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Citation'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('explication', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '50',
+                ],
+                'label' => 'Explication',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'required' => false,
+            ])
+            ->add('auteurs', EntityType::class, [
+                'class' => Auteurs::class,
+                'choice_label' => 'auteur',
+                'label' => 'Auteur',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'required' => false,
+                'placeholder' => 'Choisir un auteur',
+                'query_builder' => function (AuteursRepository $auteursRepository) {
+                    return $auteursRepository->createQueryBuilder('a')
+                        ->orderBy('a.auteur', 'ASC')
+                    ;
+                }
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' => 'Valider'
+                ]);
+            
+        ;
+    }
+
+    /**
+     * Methode qui configure les options du formulaire
+     *
+     * @param OptionsResolver $resolver
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Citations::class,
+        ]);
+    }
+}
